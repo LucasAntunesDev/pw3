901f90278c8bf8fc0ce186702a93b24413d4b358
LucasAntunesDev <lucasantuneslara06@gmail.com>
2024-02-29 09:16:43 -0300

    chore: mudanças de classes utiliatárias

--- trimestre_1/2_fevereiro/29_revisao_tailwind/bem_vindo.php
+++ trimestre_1/2_fevereiro/29_revisao_tailwind/bem_vindo.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-    
-<?php if(!isset($_POST['usuario']) || $_POST['usuario'] == null) header('location:index.php')?>
 
-<head>
+<?php if (!isset($_POST['usuario']) || $_POST['usuario'] == null)
+    header('location:index.php') ?>
+
+    <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
-    <title>Bem-vindo, <?php echo $_POST['nome'] . ' '; echo $_POST['sobrenome'];?></title>
+        <title>Bem-vindo,
+        <?php echo $_POST['nome'] . ' ';
+echo $_POST['sobrenome']; ?>
+    </title>
 </head>
 
 <body class="h-[-webkit-fill-available] h-[-moz-available] dark:bg-neutral-900">
-    
+
     <div class="flex flex-col h-screen w-6/12 mx-auto">
         <a href="index.php" class="pt-4 text-blue-500 hover:underline">Voltar ao início</a>
-        <h1 class="font-bold text-4xl py-4dark:text-zinc-50">Bem-vindo,  
-            <?php 
-                echo $_POST['nome'] . ' ';
-                echo $_POST['sobrenome'];
+        <h1 class="font-bold text-4xl py-4dark:text-zinc-50">Bem-vindo,
+            <?php
+            echo $_POST['nome'] . ' ';
+            echo $_POST['sobrenome'];
             ?>
         </h1>
 
