f6ea595cfbb72cb0115cc78ca5dce9ab8ec1610e
LucasAntunesDev <lucasantuneslara06@gmail.com>
2024-04-01 11:17:49 -0300

    teste

---
+++ trimestre_1/3_marco/twig/fruta.php
@@ -0,0 +1,23 @@
+<?php
+// fruta.php
+require('twig_carregar.php');
+
+$frutas = [
+    'Abacaxi 🍍',
+    'Laranja 🍊',
+    'Banana 🍌',
+    'Maçã 🍎',
+    'Uva 🍇',
+    'Bergamota 🍊',
+    'Melancia 🍉',
+];
+
+// Ou o valor de $_GET['fruta] ou null (caso não exista a variável ou o valor)
+$frutaEscolhida = $_GET['fruta'] ?? null;
+
+echo $twig->render('fruta.html', [
+    'frutas' => $frutas,
+    // Ou o resultado de $frutas[indice] ou null (caso não exista o índice ou tenha vindo com um índice nulo)
+    'frutaEscolhida' => $frutas[$frutaEscolhida] ?? null,
+    'titulo' => 'Frutas'
+]);
